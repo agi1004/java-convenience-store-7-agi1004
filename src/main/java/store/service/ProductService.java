03ccfa50f1718c55b521567413f8c489858140eb
agi1004 <agi1009@naver.com>
2024-11-11 16:31:44 +0900

    feat: Product 엔티티의 비즈니스 로직을 처리
    1. 모든 Product 엔티티 목록을 불러온 후 Dto로 변환하여 리턴
    2. 상품 구매 (상품 수량 줄이기)

---
+++ src/main/java/store/service/ProductService.java
@@ -0,0 +1,10 @@
+package store.service;
+
+import java.util.List;
+
+import store.dto.ProductDto;
+
+public interface ProductService {
+	List<ProductDto> getAll();
+	void purchase();
+}
---
+++ src/main/java/store/service/ProductServiceImpl.java
@@ -0,0 +1,100 @@
+package store.service;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import store.domain.Product;
+import store.domain.Order;
+import store.dto.ProductDto;
+import store.repository.ProductRepository;
+import store.repository.ProductRepositoryImpl;
+import store.repository.OrderRepository;
+import store.repository.OrderRepositoryImpl;
+
+public class ProductServiceImpl implements ProductService {
+	private static final ProductService INSTANCE;
+	private final ProductRepository productRepository;
+	private final OrderRepository orderRepository;
+	
+	static {
+		INSTANCE = new ProductServiceImpl(ProductRepositoryImpl.getInstance(),
+										  OrderRepositoryImpl.getInstance());
+	}
+	
+	private ProductServiceImpl(final ProductRepository productRepository,
+							   final OrderRepository orderRepository) {
+		this.productRepository = productRepository;
+		this.orderRepository = orderRepository;
+	}
+	
+	public static ProductService getInstance() {
+		return INSTANCE;
+	}
+	
+	@Override
+	public List<ProductDto> getAll() {
+		return productRepository.findAll().stream()
+				.map(product -> ProductDto.of(product))
+				.collect(Collectors.toList());
+	}
+	
+	@Override
+	public void purchase() {
+		Order order = orderRepository.find();
+		Map<Product, Long> payments = order.getPayments();
+		payments.keySet().stream()
+				.forEach(product -> calculate(product, payments));
+	}
+	
+	private void calculate(Product product, Map<Product, Long> payments) {
+		List<Product> stocks = productRepository.findListInStockByName(product.getName());
+		long totalPurchaseQuantity = payments.get(product);	// 전체 구매 수량
+		
+		if (hasPromotion(stocks.get(0))) {
+			handlePromotion(stocks, totalPurchaseQuantity);
+			return;
+		}
+		
+		reduceQuantity(product, totalPurchaseQuantity);
+	}
+	
+	private boolean hasPromotion(Product product) {
+		if (product.getPromotion() == null) {
+			return false;
+		}
+		
+		return true;
+	}
+	
+	private void handlePromotion(List<Product> stocks, long totalPurchaseQuantity) {
+		long remain = totalPurchaseQuantity;	// 남은 구매 수량을 초기화
+		
+		for (Product stock : stocks) {
+			long nowQuantity = stock.getQuantity(); // 현재 상품의 남은 수량
+			
+			if (isNowQuantityOverThanRemain(nowQuantity, remain, stock)) {
+				break;
+			}
+			
+			remain -= nowQuantity;
+		}
+	}
+	
+	private boolean isNowQuantityOverThanRemain(long nowQuantity, long remain, Product stock) {
+		// 현재 재고가 남은 수량보다 많거나 같으면, 남은 수량만큼 구매
+		if (nowQuantity >= remain) {
+			reduceQuantity(stock, remain);
+			return true;
+		}
+		
+		// 현재 재고가 남은 수량보다 적으면, 현재 재고만큼 구매 후 남은 수량 갱신
+		reduceQuantity(stock, nowQuantity);
+		return false;
+	}
+	
+	private void reduceQuantity(Product product, long quantity) {
+		product.reduceQuantity(quantity);
+		productRepository.save(product);
+	}
+}
