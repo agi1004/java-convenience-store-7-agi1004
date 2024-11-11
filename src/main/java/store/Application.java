784f69135bcdc16afeb4b3e104befae22b01708b
agi1004 <agi1009@naver.com>
2024-11-11 15:39:46 +0900

    feat: 클라이언트 코드
    Controller만 의존하기 때문에 클라이언트는 내부 구현을 몰라도 프로그램 사용 가능

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -1,7 +1,11 @@
 package store;
 
+import store.controller.Controller;
+import store.controller.ControllerImpl;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+       Controller controller = ControllerImpl.getInstance();
+       controller.run();
     }
 }
