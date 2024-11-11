07d527c23efe9e0959a00e6213e7ba0e5c6c9f4c
agi1004 <agi1009@naver.com>
2024-11-11 16:13:29 +0900

    숫자 상수 클래스

---
+++ src/main/java/store/constant/Number.java
@@ -0,0 +1,12 @@
+package store.constant;
+
+public final class Number {
+	public static final int HEADER_LINE = 1;
+	public static final int BOTH_END_INDEXES = 1;
+	public static final int FIRST_PRODUCT_INDEX = 0;
+	public static final int ZERO = 0;
+	public static final int MAX_MEMBERSHIP_LIMIT = 8000;
+	public static final double MEMBERSHIP_DISCOUNT_PERCENT = 0.3;
+	public static final int UNIT = 1000;
+	public static final String NULL = "null";
+}
